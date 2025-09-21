c6e9291b10ad3102398824f160e1172bc30ba901
lh <67316195+zerozeroone001@users.noreply.github.com>
2025-09-21 10:36:25 +0800

    9-21

--- RuoYi-Vue-master/ruoyi-system/src/main/java/com/ruoyi/system/mapper/SysMeetingTopicMapper.java
+++ RuoYi-Vue-master/ruoyi-system/src/main/java/com/ruoyi/system/mapper/SysMeetingTopicMapper.java
@@ -3,7 +3,7 @@ package com.ruoyi.system.mapper;
 
 import java.util.List;
 import com.ruoyi.system.domain.SysMeetingTopic;
-import io.lettuce.core.dynamic.annotation.Param;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * 会议议题Mapper接口
